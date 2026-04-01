08f56a2334806a5f767ee1d47e9a665f8b1f79ca
Lenovo08 <sdfsd>
2026-04-01 18:48:14 +0400

    1

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return """<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <img src="../img/Michael.jpg" alt="Здесь должна быть картинка">
+</head>
+<body>
+</body>
+</html>"""
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 7070))
+    app.run(host='0.0.0.0', port=port)
+
